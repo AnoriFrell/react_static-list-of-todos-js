d5f3951f7dab3c62227689a93f97b1d553d17109
AnoriFrell <anori.fjord@gmail.com>
2025-05-13 21:07:15 +0300

    list

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,2 +1,19 @@
+import { UserInfo } from '../UserInfo';
+
 // Add the required props
-export const TodoInfo = () => <>TodoInfo markup</>;
+export const TodoInfo = ({ todo, users }) => {
+  const { userId, completed, title } = todo;
+
+  return (
+    <article
+      className={completed ? 'TodoInfo TodoInfo--completed' : 'TodoInfo'}
+    >
+      <h2 className="TodoInfo__title">{title}</h2>
+      {users.map(
+        user =>
+          user.id === userId ? <UserInfo user={user} key={user.id} /> : null,
+        // eslint-disable-next-line function-paren-newline
+      )}
+    </article>
+  );
+};
--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,2 +1,12 @@
+import { TodoInfo } from '../TodoInfo';
+
 // Add the required props
-export const TodoList = () => <>TodoList markup</>;
+export const TodoList = ({ todos, users }) => {
+  return (
+    <section className="TodoList">
+      {todos.map(todo => {
+        return <TodoInfo key={todo.id} todo={todo} users={users} />;
+      })}
+    </section>
+  );
+};
